5fe444fcfcc2c6fdd322839f20d542f14aa726d8
Kwaku-code <kwaku.agyapong@amalitech.com>
2022-03-04 15:49:57 +0000

    Post requst amended

--- index.js
+++ index.js
@@ -6,19 +6,19 @@ app.use(express.json());
 
 const genres = [ 
     {movieId: 1, movieGenre: 'action'},
-    {movieId: 1, movieGenre: 'comedy'},
-    {movieId: 1, movieGenre: 'family'} 
+    {movieId: 2, movieGenre: 'comedy'},
+    {movieId: 3, movieGenre: 'family'}
 ];
 
 function validateMovie(genre) {
     const schema = {
-        movieGenre : Joi.string.min(2).required()
+        movieGenre : Joi.string().min(2).required()
     };
 
     return Joi.validate(genre, schema);
 };
 
-app.post('/api/genres/:movieId', (req, res) => {
+app.post('/api/genres', (req, res) => {
     const { error } = validateMovie(req.body);
     if (error) return res.status(400).send(error.details[0].message);
 
@@ -31,7 +31,7 @@ app.post('/api/genres/:movieId', (req, res) => {
 });
 
 app.put('/api/genres/:movieId', (req, res) => {
-    const genre = genres.find(m => m.movieId == (req.params.movieId));
+    const genre = genres.find(m => m.movieId == parseInt(req.params.movieId));
     if (!genre) return res.status(404).send('The genre with the given id was not found.');
 
     const { error } = validateMovie(req.body);
@@ -41,20 +41,25 @@ app.put('/api/genres/:movieId', (req, res) => {
     res.send(genre);
 });
 
-app.delete('/api/genres/:movieId', (req, req) => {
-    const genre = genres.find(m => m.movieId == (req.params.movieId));
+app.delete('/api/genres/:movieId', (req, res) => {
+    const genre = genres.find(m => m.movieId == parseInt(req.params.movieId));
     if (!genre) return res.status(404).send('The genre with the given id was not found.');
 
     const index = genres.indexOf(genre);
-    genres.splice(index, 1);
+    genres.splice(index, 1)
 
     res.send(genre);
 });
 
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+});
+
 app.get('/api/genres/:movieId', (req, res) => {
-    const genre = genres.find(m => m.movieId == (req.params.movieId));
+    const genre = genres.find(m => m.movieId == parseInt(req.params.movieId));
     if (!genre) return res.status(404).send('The genre with the given id was not found.');
     res.send(genre);
 });
 
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
