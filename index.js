e5d61c18b2ebc9e8bce614201fa9091ae201a84d
Kwaku  Agyapong <kwaku.agyapong@amalitech.org>
2022-03-04 10:04:09 +0000

    Movie genre Post and Update requests added

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ const app = express();
 
 app.use(express.json());
 
-const movies = [ 
-    {movieId: 1, genre: 'action'},
-    {movieId: 1, genre: 'comedy'},
-    {movieId: 1, genre: 'family'} 
+const genres = [ 
+    {movieId: 1, movieGenre: 'action'},
+    {movieId: 1, movieGenre: 'comedy'},
+    {movieId: 1, movieGenre: 'family'} 
 ];
 
-function validateMovie(movie) {
+function validateMovie(genre) {
     const schema = {
-        genre : Joi.string.min(2).required()
+        movieGenre : Joi.string.min(2).required()
     };
 };
 
-app.post('/api/movies/')
+app.post('/api/genres/:movieId', (req, res) => {
+    const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        movieId: genres.length + 1,
+        movieGenre: req.body.movieGenre
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
+app.put('/api/genres/:movieId', (req, res) => {
+    const genre = genres.find(m => m.movieId == (req.params.movieId));
+    if (!genre) return res.status(404).send('The genre with the given id was not found.');
+
+    const { error } = validateMovie(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    genre.movieGenre = req.body.movieGenre;
+    res.send(genre);
+});
