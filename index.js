39e2fe1c831473ee7c3d3f5ea007af43e6992b09
Kwaku-code <kwaku.agyapong@amalitech.com>
2022-03-28 17:16:22 +0000

    User Authentication added

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+const config = require('config');
 const Joi = require('Joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
-const users = require('./routes/users');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
 const movies = require('./routes/movies');
+const users = require('./routes/users');
+const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+if (!config.get('jwtPrivateKey')) {
+    console.error('FATAL ERROR: jwtPrivateKey is not defined.');
+    process.exit(1);
+}
 
 mongoose.connect('mongodb://localhost/vidly')
     .then(() => console.log('Connected to MongoDB...'))
@@ -19,6 +26,7 @@ app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
 app.use('/api/users', users);
+app.use('/api/auth', auth);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
