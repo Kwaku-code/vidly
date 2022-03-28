0997787ae15008ca6ee424b7372627f55e435188
Kwaku-code <kwaku.agyapong@amalitech.com>
2022-03-28 13:41:42 +0000

    User model and routes added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Joi = require('Joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const mongoose = require('mongoose');
+const users = require('./routes/users');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const rentals = require('./routes/rentals');
@@ -17,6 +18,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
