5621e92181089cf1117b694ed46d0b3686926771
Kwaku-code <kwaku.agyapong@amalitech.com>
2022-04-05 13:53:04 +0000

    Vidly application refactor

--- middleware/error.js
+++ middleware/error.js
@@ -4,5 +4,12 @@ module.exports = function(err, req, res, next) {
     // winston.log('error', err.message);
     winston.error(err.message, err);
 
+    // error
+    // warn
+    // info
+    // verbose
+    // debug
+    // silly
+
     res.status(500).send('Something failed.');
 }
