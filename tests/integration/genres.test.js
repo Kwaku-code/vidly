c0d579c84801d995c21dac372a2038513490179d
Kwaku-code <kwaku.agyapong@amalitech.com>
2022-04-20 13:48:18 +0000

    Coverage for tests added to vidly

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -15,8 +15,8 @@ describe('/api/genres', () => {
     describe('GET /', () => {
         it('should return all genres', async () => {
             await Genre.collection.insertMany([
-                { name: 'genre1'},
-                { name: 'genre2'}
+                { name: 'genre1' },
+                { name: 'genre2' }
             ]);
  
             const res = await request(server).get('/api/genres');
@@ -24,7 +24,7 @@ describe('/api/genres', () => {
             expect(res.status).toBe(200);
             expect(res.body.length).toBe(2);
             expect(res.body.some(g => g.name === 'genre1')).toBeTruthy();
-            expect(res.body.some(g => g.name === 'genre2')).toBeTruthy();
+            expect(res.body.some(g => g.name=== 'genre2')).toBeTruthy();
         });
     });
 
@@ -43,6 +43,13 @@ describe('/api/genres', () => {
     describe('GET /:id', () => {
         it('should return 404 if invalid id is passed', async () => {
             const res = await request(server).get('/api/genres/1');
+
+            expect(res.status).toBe(404);
+        });
+
+        it('should return 404 if no genre with the given id exists', async () => {
+            const id = mongoose.Types.ObjectId();
+            const res = await request(server).get('/api/genres/' + id);
 
             expect(res.status).toBe(404);
         });
